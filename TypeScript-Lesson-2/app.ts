e0098300d0b2b22b14dadfcb0bdc337cf154fcc1
ProgrammingPark <90354391+ProgrammingPark@users.noreply.github.com>
2021-11-03 22:16:13 +0200

    Add the 2nd TypeScript lesson

---
+++ TypeScript-Lesson-2/app.ts
@@ -0,0 +1,26 @@
+let temperature: number = 2;
+let year = 2016;
+let percentage = 53.2;
+
+console.log(`Type of temperature: ${typeof temperature}`);
+console.log(`Temperature is: ${temperature}`);
+console.log(typeof year);
+console.log(`${typeof percentage}\n`);
+
+let isCold: boolean = false;
+
+console.log(typeof isCold);
+
+let name: string = "Denis";
+let firstLetter = 'a';
+
+console.log(typeof name);
+console.log(typeof firstLetter);
+
+
+let temp: any;
+
+temp = 2;
+
+console.log(typeof temp);
+console.log("\n");
